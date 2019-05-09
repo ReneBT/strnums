f371097720538cf3dcbc0d2a870d7ac7f921ee07
Rene Beattie <36237381+ReneBT@users.noreply.github.com>
2019-05-09 04:19:51 +0100

    Add files via upload
    Updated to handles strings with no numbers present and strings with no letters present

--- strnums.m
+++ strnums.m
@@ -1,4 +1,4 @@
-function numfromstr = strnums(tempstr,opt,sep,real)
+function [ numfromstr, txtfromstr ] = strnums( tempstr , opt , sep , real )
 % strnums is a function to scan through a string and isolate just the
 % numbers before converting to a number.
 % INPUTS:   tempstr:    the string containing mixed numeric and non-numeric
@@ -20,6 +20,11 @@ function numfromstr = strnums(tempstr,opt,sep,real)
 %           separator characters, but grouping characters according
 %           the characters listed in sep
 %       4. Return each digit separately
+%
+% OUTPUTS:  numfromstr: array of numeric output, double or double
+%                       imaginary if i or j present and real=false
+%           txtfromstr: cell array of strings preceeding or following
+%                       numeric groups.
 %
 % num = strnums('A123 231'); gives num = 123231
 % num = strnums('A123 231',1); gives num = 123231
@@ -84,16 +89,16 @@ function numfromstr = strnums(tempstr,opt,sep,real)
 % modified 13/03/2019 to handle mathematical format characters +,- and . as
 % well as handling i and j as imaginary numbers
 
-if nargin<4 %default to ignoring imaginary number
+if nargin < 4 %default to ignoring imaginary number
     real = true;
 end
-if nargin<3
+if nargin < 3
     sep = ' '; %default seperator to space
 end
-if nargin<2
+if nargin < 2
     opt = 1; %default option is return as all one number, dropping leading zeros
 end
-if ~any(opt ==1:4)
+if ~any( opt == 1:4 )
     error(sprintf(['options are\n1 for returning concatenated number\n2 ',...
         'for bunched numbers (i.e. consecutive numeric digits treated as ',...
         'one number)\n3 for multiple digits where spaces exist\n4 for every ',...
@@ -101,73 +106,102 @@ if ~any(opt ==1:4)
 end
 
 %initialise nustr variable to store the numeric strings
-nustr = [];
-
+nustr = [ ];
+    
 if real
-    numChar = {'0','1','2','3','4','5','6','7','8','9'};
+    numChar = { '0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9'};
 else
-    numChar = {'0','1','2','3','4','5','6','7','8','9','i','j'};
+    numChar = { '0' , '1' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' ,...
+        'i' , 'j' };
 end
-numForm = {'+','-','.'}; %note does not currently interpret commas as decimal point
+numForm = { '+' , '-' , '.' }; %note does not currently interpret commas as decimal point
 
-if iscell(sep)
-    for iS = 1:length(sep)
-        numForm = numForm(~strcmp(sep{iS},numForm)); %if defined as a separator, overule number format
+if iscell( sep )
+    for iS = 1:length( sep )
+        numForm = numForm( ~strcmp( sep{iS} , numForm ) ); %if defined as a separator, overule number format
     end
 else
-    numForm = numForm(~strcmp(sep,numForm)); %if defined as a separator, overule number format
+    numForm = numForm( ~strcmp( sep , numForm ) ); %if defined as a separator, overule number format
 end
 
-for iD = 1:length(tempstr)
-    if any(strcmp(tempstr(iD),[numChar,numForm])) %exclude spaces to make one number
-        if any(strcmp(tempstr(iD),{'i','j'})) && ...
-                any(strcmp(tempstr(iD-1),[numChar(1:10),numForm]))                
+for iD = 1:length( tempstr )
+    if any( strcmp( tempstr( iD ) , [ numChar , numForm] ) ) %exclude spaces to make one number
+        if any( strcmp( tempstr( iD ) , {'i' , 'j'} ) ) && ...
+                any( strcmp( tempstr( iD-1 ) , [ numChar( 1:10 ) , numForm] ) )                
             % imaginary i should be proceeded by space, plus,
             % minus or real number
             switch opt
                 case 4
-                    if any(strcmp(tempstr(iD),numChar(1:10)))
-                        nustr = [nustr,tempstr(iD),' ']; %#ok<*AGROW>
+                    if any( strcmp( tempstr( iD ) , numChar( 1:10 ) ) )
+                        nustr = [ nustr , tempstr( iD ) , ' ']; %#ok<*AGROW>
+                        tempstr( iD ) = char(1029); %remove numbers once read
                     else
-                        nustr = [nustr,' ']; 
+                        nustr = [ nustr , ' ']; 
                     end
                 otherwise
-                    nustr = [nustr,tempstr(iD)];
+                    nustr = [ nustr , tempstr( iD )];
+                    tempstr( iD ) = char(1029); %remove numbers once read
             end
-        elseif ~any(strcmp(tempstr(iD),{'i','j'}))
+        elseif ~any( strcmp( tempstr( iD ) , {'i' , 'j'} ) )
             switch opt
                 case 4
-                    if any(strcmp(tempstr(iD),numChar(1:10)))
-                        nustr = [nustr,tempstr(iD),' '];
+                    if any( strcmp( tempstr( iD ) , numChar( 1:10 ) ) )
+                        nustr = [ nustr , tempstr( iD ) , ' '];
+                        tempstr( iD ) = char(1029); %remove numbers once read
                     else
-                        nustr = [nustr,' '];
+                        nustr = [ nustr , ' '];
                     end
                 otherwise
-                    nustr = [nustr,tempstr(iD)];
+                    nustr = [ nustr , tempstr( iD )];
+                    tempstr( iD ) = char(1029); %remove numbers once read
             end  
         elseif opt==2
-            nustr = [nustr,' ']; %separate out numbers split by anything non-numeric
+            nustr = [ nustr , ' ']; %separate out numbers split by anything non-numeric
         end
-    elseif opt == 3 && any(strcmp(tempstr(iD),sep))
-        nustr = [nustr,' ']; %separate out numbers split by defined separators
+    elseif opt == 3 && any( strcmp( tempstr( iD ) , sep ) )
+        nustr = [ nustr , ' ']; % separate out numbers split by defined separators
+        tempstr( iD ) = char(1029); %  remove separators once read
     elseif opt ==2
-        nustr = [nustr,' ']; %separate out numbers split by anything non-numeric        
+        nustr = [ nustr , ' ']; %separate out numbers split by anything non-numeric   
     end
 end
 
-
-if ~isempty(nustr)
-    numfromstr = str2num(nustr); %#ok<ST2NM>
-    if isempty(numfromstr)
+if ~isempty( nustr )
+    numfromstr = str2num( nustr ); %#ok<ST2NM>
+    if isempty( numfromstr )
         nForms = 0;
-        for iF = 1:length(numForm)
-            nForms = nForms+numel(strfind(nustr,numForm{iF}));
+        for iF = 1:length( numForm )
+            nForms = nForms+numel( strfind( nustr , numForm{iF} ) );
         end
         if nForms>1
-            warning(['more than one format character present in extracted ',...
-                'string, try option 2 or 3 to separate out each numeric group'])
+            warning( [ 'more than one format character present in extracted ',...
+                'string, try option 2 or 3 to separate out each numeric group' ] )
         end
     end
 else
     numfromstr = NaN;
+end
+
+spcs = strfind(tempstr,char(1029));
+cons = [2,spcs(2:end)-spcs(1:end-1)]; %first value means that leading marker retained
+spcs(cons==1) = [];
+txtfromstr = cell(0);
+if isempty(spcs)
+    warning('STRNUMS:NONUMBER','No number was found in the string')
+    txtfromstr{1} = tempstr;
+end
+if spcs(1) ~=1 %if first character is not a digit
+    spcs = [0,spcs];
+end
+for iSp = 2:numel( spcs )
+    temp = tempstr( spcs ( iSp-1 ) +1 : spcs( iSp ) -1 );
+    temp( strfind( temp , char( 1029 ) ) ) = [ ]; %remove separator characters
+    txtfromstr{ iSp-1 } = temp; %take string between spacers
+end
+if spcs( end ) ~= length( tempstr ) && ~isequal( spcs, 1)
+    temp = tempstr( spcs( iSp ) +1 : end );
+    temp( strfind( temp , char( 1029 ) ) ) = [ ]; %remove separator characters
+    txtfromstr{ iSp } = temp;
+else
+    txtfromstr{1} = '';
 end
